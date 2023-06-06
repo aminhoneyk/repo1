e9db43056b593203eee7ff4e22e17d0fa82eeaf4
aminhoneyk <124121288+aminhoneyk@users.noreply.github.com>
2023-06-06 23:24:12 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+export function Second() {
+  return(
+    console.log("Hiiiiiiiii")
+
+  ); 
+}
+export default function Main_component()
+{
+  return(
+  <section>
+   <h1>hello--------</h1>
+  </section>
+  );
+}
